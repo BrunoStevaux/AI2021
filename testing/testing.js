05e0db54b97aec58af5abded14fe8c6787e7276e
Keegan222 <keegan742@gmail.com>
2021-11-22 19:18:45 -0400

    .

---
+++ testing/testing.js
@@ -0,0 +1,67 @@
+let classifier;
+let canvas;
+let resultsDiv;
+let inputImage;
+let clearButton;
+
+function setup() {
+    // Create canvas
+    canvas = createCanvas(400, 400);
+
+    // Initialize model to load into
+    let options = {
+        inputs: [28, 28, 4],
+        task: 'imageClassification',
+    };
+    classifier = ml5.neuralNetwork(options);
+    const modelDetails = {
+        model: 'trained_model/model.json',
+        metadata: 'trained_model/model_meta.json',
+        weights: 'trained_model/model.weights.bin',
+    };
+
+    // Create buttons
+    background(255);
+    clearButton = createButton('clear');
+    clearButton.mousePressed(function() {
+        background(255);
+    });
+    resultsDiv = createDiv('loading model');
+    inputImage = createGraphics(28, 28);
+
+    // Load the model
+    classifier.load(modelDetails, modelLoaded);
+}
+
+function modelLoaded() {
+    console.log('model ready!');
+    classifyImage();
+}
+
+function classifyImage() {
+    // Attempt to classify an image
+    inputImage.copy(canvas, 0, 0, 400, 400, 0, 0, 28, 28);
+    image(inputImage, 0, 0);
+    classifier.classify({ image: inputImage }, gotResults);
+}
+
+function gotResults(err, results) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    let label = results[0].label;
+    let confidence = nf(100 * results[0].confidence, 2, 0);
+
+    resultsDiv.html(`${label} ${confidence}%`);
+
+    classifyImage();
+}
+
+function draw() {
+    if (mouseIsPressed) {
+        strokeWeight(8);
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+}
---
+++ training/training.js
@@ -0,0 +1,41 @@
+let images = [9];
+
+const counts = [5923, 6742, 5957, 6131, 5842, 5421, 5918, 6265, 5851, 5948];
+
+function preload() {
+    for (i = 0; i < 10; i++) {
+        images[i] = [];
+        // The upper bound on j must change to load more data from each digit
+        for (j = 0; j < 100; j++) {
+            console.log(`Loading ${i} image ${j}`);
+            images[i][j] = loadImage(`./training_data/${i}-${j}.png`);
+        }
+    }
+}
+
+let classifier;
+
+function setup() {
+    console.log(images[0].length);
+
+    let options = {
+        inputs: [28, 28, 4],
+        task: 'imageClassification',
+        debug: true,
+    };
+    classifier = ml5.neuralNetwork(options);
+
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < images[i].length; j++) {
+            console.log(`Adding ${i} image ${j}`);
+            classifier.addData({ image: images[i][j] }, { label: i });
+        }
+    }
+    classifier.normalizeData();
+    classifier.train({ epochs: 50 }, finishedTraining);
+}
+
+function finishedTraining() {
+    console.log('Finished training');
+    classifier.save();
+}
