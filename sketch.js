00a31df3ff793eb2a5af723f1b7e8edd4d7283b1
BrunoStevaux <144894s@acadiau.ca>
2021-11-22 18:33:57 -0400

    100 images at a time

--- sketch.js
+++ sketch.js
@@ -3,10 +3,10 @@ let images = [9];
 const counts = [5923, 6742, 5957, 6131, 5842, 5421, 5918, 6265, 5851, 5948];
 
 function preload() {
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++) {  
         images[i] = [];
         // The upper bound on j must change to load more data from each digit
-        for (j = 0; j < 10; j++) {
+        for (j = 0; j < 100; j++) {
             images[i][j] = loadImage(`./training_data/${i}-${j}.png`);
         }
     }
